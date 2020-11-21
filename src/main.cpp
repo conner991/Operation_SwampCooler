d773e2583633192878c90c6c8d04075fed13ca68
conner991 <32372555+conner991@users.noreply.github.com>
2020-11-20 19:59:09 -0800

    tester

--- src/main.cpp
+++ src/main.cpp
@@ -3,14 +3,13 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 #include <SimpleDHT.h>
-#include <dht.h>
 
 volatile unsigned char *myADCSRA = (unsigned char *)0x7A;
 volatile unsigned char *myADCSRB = (unsigned char *)0x7B;
 volatile unsigned char *myADMUX = (unsigned char *)0x7C;
 volatile unsigned int *myADCDATA = (unsigned int *)0x78;
 
-// Define Port B Register Pointers
+// Define Port Register Pointers
 volatile unsigned char *portB = (unsigned char *)0x25;
 volatile unsigned char *ddrB = (unsigned char *)0x24;
 volatile unsigned char *pinB = (unsigned char *)0x23;
@@ -32,111 +31,107 @@ void adcInit();
 unsigned int adcRead(unsigned char adcChannelNum);
 void displayWaterLevel(unsigned int waterLevel);
 
+// Global Variables
 unsigned int waterLevel = 0;
 unsigned int tempValue = 0;
-
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-dht DHT;
-#define dht_apin A0
+LiquidCrystal lcd(3, 4, 5, 6, 7, 8);
+const int pinDHT11 = 2;
+SimpleDHT11 dht11; //instantiates the SimpleDHT11 Object class to variable dht11
+float temperatureC = 0.0;
+float temperatureF = 0.0;
+float humidity = 0.0;
 
 void setup()
 {
   // set up the ADC
   adcInit();
   Serial.begin(9600);
+  lcd.begin(16, 2);
 
   // set PB7 to output lights
   *ddrB = 0xF0;
-
-  lcd.begin(16, 2);
-
 }
 
 void loop()
 {
-  // Get the reading from the ADC
-  unsigned int waterLevel = adcRead(0);
 
-  // Delay
-  //delay(100);
+  //Local data array
+  byte data[40] = {0};
+  if (dht11.read2(pinDHT11, &temperatureC, &humidity, data))
+  {
+    Serial.print("Read DHT11 failed");
+    return;
+  }
 
-  // reads in the water level and displays it
-  displayWaterLevel(waterLevel);
+  Serial.print("Sample RAW Bits: ");
+  for (int i = 0; i < 40; i++)
+  {
+    Serial.print((int)data[i]);
+    if (i > 0 && ((i + 1) % 4) == 0)
+    {
+      Serial.print(' ');
+    }
+  }
 
+  Serial.println("");
+  //°F = °C x 9/5 + 32.
+  temperatureF = temperatureC * 9.0 / 5.0 + 32.0;
+  Serial.print("Sample OK: ");
+  Serial.print(temperatureC);
+  Serial.print("°C, ");
+  Serial.print(temperatureF);
+  Serial.print("°F, ");
+  Serial.print(humidity);
+  Serial.println("%");
 
-  int chk = DHT.read11(dht_apin); 
-  lcd.setCursor(0,0); 
-  lcd.print("Temp: ");
-  lcd.print(DHT.temperature);
-  lcd.print((char)223);
-  lcd.print("C");
-  lcd.setCursor(0,1);
-  lcd.print("Humidity: ");
-  lcd.print(DHT.humidity);
-  lcd.print("%");
-  delay(2000);
+  // DHT11 sampling rate is 1HZ.
+  delay(1000);
 
+  //   // Get the reading from the ADC
+  //   unsigned int waterLevel = adcRead(0);
+  //
+  //   // Delay
+  //   //delay(100);
+  //
+  //   // reads in the water level and displays it
+  //   displayWaterLevel(waterLevel);
 }
 
 void adcInit()
 {
   // Set up the A register
-  // Set bit 7 to 1 to enable the ADC
-  *myADCSRA |= 0x80;
-  // clear bit 5 to 0 to disable the ADC trigger mode
-  *myADCSRA &= 0xDF;
-  // clear bit 3 to 0 to disable the ADC interrupt
-  *myADCSRA &= 0xF7;
-  // clear bits 2-0 to 0 to set prescalar selection to slow reading (Division Factor of 2)
-  *myADCSRA &= 0xF8;
+  *myADCSRA |= 0x80; // Set bit 7 to 1 to enable the ADC
+  *myADCSRA &= 0xDF; // clear bit 5 to 0 to disable the ADC trigger mode
+  *myADCSRA &= 0xF7; // clear bit 3 to 0 to disable the ADC interrupt
+  *myADCSRA &= 0xF8; // clear bits 2-0 to 0 to set prescalar selection to slow reading (Division Factor of 2)
 
   // Setup the B register
-  // clear bit 3 to 0 to reset the channel and gain bits
-  *myADCSRB &= 0xF7;
-  // clear bits 2-0 to 0 to set free running mode
-  *myADCSRB &= 0xF8;
+  *myADCSRB &= 0xF7; // clear bit 3 to 0 to reset the channel and gain bits
+  *myADCSRB &= 0xF8; // clear bits 2-0 to 0 to set free running mode
 
   // set up the MUX register
-  // clear bit 7 to 0 for AREF, internal Vref turned off
-  *myADMUX &= 0x7F;
-  // set bit 6 to 1 for AVCC analog reference, external capacitor at AREF pin
-  *myADMUX |= 0x40;
-  // clear bit 5 to 0 for right adjust result
-  *myADMUX &= 0xDF;
-  // clear bits 4-0 to 0 to reset the channel and gain bits
-  *myADMUX &= 0xE0;
+  *myADMUX &= 0x7F; // clear bit 7 to 0 for AREF, internal Vref turned off
+  *myADMUX |= 0x40; // set bit 6 to 1 for AVCC analog reference, external capacitor at AREF pin
+  *myADMUX &= 0xDF; // clear bit 5 to 0 for right adjust result
+  *myADMUX &= 0xE0; // clear bits 4-0 to 0 to reset the channel and gain bits
 }
 
 unsigned int adcRead(unsigned char adcChannelNum)
 {
-  // clear the channel selection bits (MUX 4:0)
-  *myADMUX &= 0xE0;
-
-  // clear the channel selection bits (MUX 5)
-  *myADCSRB &= 0xF7;
-
-  // set the channel number
-  if (adcChannelNum > 7)
+  *myADMUX &= 0xE0;      // clear the channel selection bits (MUX 4:0)
+  *myADCSRB &= 0xF7;     // clear the channel selection bits (MUX 5)
+  if (adcChannelNum > 7) // set the channel number
   {
-    // set the channel selection bits, but remove the most significant bit (bit 3)
-    adcChannelNum -= 8;
-
-    // Set MUX 5
-    *myADCSRB |= 0x08;
+    adcChannelNum -= 8; // set the channel selection bits, but remove the most significant bit (bit 3)
+    *myADCSRB |= 0x08;  // Set MUX 5
   }
 
-  // Set the channel selection bits
-  *myADMUX += adcChannelNum;
-
-  // Set bit 6 of ADCSRA to 1 to start a conversion
-  *myADCSRA |= 0x40;
-
-  // Wait for the conversion to complete
-  while ((*myADCSRA & 0x40) != 0)
+  *myADMUX += adcChannelNum;      // Set the channel selection bits
+  *myADCSRA |= 0x40;              // Set bit 6 of ADCSRA to 1 to start a conversion
+  while ((*myADCSRA & 0x40) != 0) // Wait for the conversion to complete
     ;
 
-  // return the result in the ADC data register
-  return *myADCDATA;
+  return *myADCDATA; // return the result in the ADC data register
 }
 
 void displayWaterLevel(unsigned int waterLevel)
